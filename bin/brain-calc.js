67e9bfd19ca31232c58f8986c058fec724cf7b98
abr-ya <gamer05@list.ru>
2021-07-27 18:39:24 +0300

    feat: Calc game - create logic

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -4,6 +4,6 @@ import gameCalc from '../src/gameCalc.js';
 
 console.log('Welcome to the Brain Games!');
 const name = hello();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log('What is the result of the expression?');
 const gameRes = gameCalc();
 console.log(`${gameRes ? 'Congratulations' : 'Let\'s try again'}, ${name}!`);
--- src/gameCalc.js
+++ src/gameCalc.js
@@ -3,9 +3,17 @@ import game from './game.js';
 import { getRandomInt } from './utils.js';
 
 const evenStep = () => {
-  const value = getRandomInt(1, 20);
-  const corAns = value % 2 === 0 ? 'yes' : 'no';
-  console.log('Question: ', value);
+  const numX = getRandomInt(1, 10);
+  const numY = getRandomInt(1, 10);
+  const opNum = getRandomInt(0, 3); // верх не входит!
+  const sign = ['+', '-', '*'][opNum];
+  const func = [
+    (a, b) => a + b,
+    (a, b) => a - b,
+    (a, b) => a * b,
+  ][opNum];
+  const corAns = func(numX, numY).toString();
+  console.log('Question: ', `${numX} ${sign} ${numY}`);
   const answer = readlineSync.question('Your answer:  ');
   const res = answer === corAns;
   console.log(res ? 'Correct!' : `'${answer}' is wrong answer ;(. Correct answer was '${corAns}'.`);
